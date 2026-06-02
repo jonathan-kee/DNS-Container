53e44bbca3f433cf3182a877d6434fec9163accc
jonathan kee <jonathankee99@gmail.com>
2026-06-03 05:38:03 +0800

    add installDocker script

---
+++ linux/selfhost-runner/scripts/installDocker.sh
@@ -0,0 +1,12 @@
+# Docker installation
+# https://docs.docker.com/engine/install/centos/#install-using-the-repository
+
+sudo dnf -y install dnf-plugins-core
+
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl enable --now docker
+
+sudo docker run hello-world
